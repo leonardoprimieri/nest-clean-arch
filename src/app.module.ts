fa9c05ca07be32b5a6acc64faf9af12350ad8cbd
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-12-25 21:20:31 -0300

    feat: add zod validation for env schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { PasswordEncryptionService } from './infra/password-hash/password-hash';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './env';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: envSchema.parse,
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService, PasswordEncryptionService],
 })
